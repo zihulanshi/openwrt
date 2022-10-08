2b58082f120af8a577fd0fcfbcbccde3cd1ce734
zihulanshi <58834622+zihulanshi@users.noreply.github.com>
2022-10-08 19:34:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -42,20 +42,21 @@ git clone https://github.com/kenzok78/luci-theme-argonne.git package/luci-theme-
 #ttyd lede包已有
 
 #  磁盘管理
-git clone https://github.com/lisaac/luci-app-diskman.git package/luci-app-diskman
-#echo 'src-git diskman https://github.com/lisaac/luci-app-diskman' >>feeds.conf.default
+#git clone https://github.com/lisaac/luci-app-diskman.git package/luci-app-diskman
+echo 'src-git diskman https://github.com/lisaac/luci-app-diskman' >>feeds.conf.default
+
 #文件传输 lede包已有
 
 #  关机
-git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
-#echo 'src-git poweroff https://github.com/esirplayground/luci-app-poweroff' >>feeds.conf.default
+#git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
+echo 'src-git poweroff https://github.com/esirplayground/luci-app-poweroff' >>feeds.conf.default
 ##############################################################################################
 
 # 服务
 
 # aliddns
-git clone https://github.com/chenhw2/luci-app-aliddns.git package/luci-app-aliddns
-
+#git clone https://github.com/chenhw2/luci-app-aliddns.git package/luci-app-aliddns
+echo 'src-git aliddns https://github.com/chenhw2/luci-app-aliddns' >>feeds.conf.default
 #docker
 #git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
 #echo 'src-git dockerman https://github.com/lisaac/luci-app-dockerman' >>feeds.conf.default
@@ -77,17 +78,19 @@ git clone https://github.com/chenhw2/luci-app-aliddns.git package/luci-app-alidd
 #git clone https://github.com/fw876/helloworld.git package/helloworld
 
 #openclash
-git clone https://github.com/vernesong/OpenClash.git package/OpenClash
-#echo 'src-git OpenClash https://github.com/vernesong/OpenClash' >>feeds.conf.default
+#git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+echo 'src-git OpenClash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 
 #luci-app-adguardhome
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 
 # UnblockNeteaseMusic
-git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/unblockmusic
+#git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
+echo 'src-git unblockmusic https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic' >>feeds.conf.default
 
 # serverchan 微信/Telegram 推送的插件
-git clone https://github.com/tty228/luci-app-serverchan.git package/serverchan
+#git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+echo 'src-git serverchan https://github.com/tty228/luci-app-serverchan' >>feeds.conf.default
 
 ##############################################################################################
 
